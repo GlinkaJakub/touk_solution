a75ba41e625b7e2f44e33279094735399d8b1635
Jakub Glinka <glinka.jakub@outlook.com>
2021-06-04 14:12:54 +0200

    Initial commit

---
+++ src/main/java/com/glinka/touk_task/ToukSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.glinka.touk_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToukSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToukSolutionApplication.class, args);
+    }
+
+}
